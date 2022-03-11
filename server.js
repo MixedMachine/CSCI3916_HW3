53ecbe041fd06d1b67a3c846f3ba368d63fa89d3
Michael Marinez <80479622+MixedMachine@users.noreply.github.com>
2022-03-10 18:32:49 -0700

    from HW2

--- server.js
+++ server.js
@@ -4,33 +4,36 @@ File: Server.js
 Description: Web API scaffolding for Movie API
  */
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var authController = require('./auth');
-var authJwtController = require('./auth_jwt');
-var jwt = require('jsonwebtoken');
-var cors = require('cors');
-var User = require('./Users');
+let express = require('express');
+let http = require('http');
+let bodyParser = require('body-parser');
+let passport = require('passport');
+let authController = require('./auth');
+let authJwtController = require('./auth_jwt');
+db = require('./db')(); //hack
+let jwt = require('jsonwebtoken');
+let cors = require('cors');
 
-var app = express();
+let app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 
-var router = express.Router();
+let router = express.Router();
 
 function getJSONObjectForMovieRequirement(req) {
-    var json = {
+    let json = {
+        status: 200,
         headers: "No headers",
-        key: process.env.UNIQUE_KEY,
-        body: "No body"
+        env: process.env.UNIQUE_KEY,
+        query: "No query",
+        query: "No query"
     };
 
-    if (req.body != null) {
-        json.body = req.body;
+    if (req.query != null) {
+        json.query = req.body;
     }
 
     if (req.headers != null) {
@@ -44,46 +47,102 @@ router.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
         res.json({success: false, msg: 'Please include both username and password to signup.'})
     } else {
-        var user = new User();
-        user.name = req.body.name;
-        user.username = req.body.username;
-        user.password = req.body.password;
+        let newUser = {
+            username: req.body.username,
+            password: req.body.password
+        };
 
-        user.save(function(err){
-            if (err) {
-                if (err.code == 11000)
-                    return res.json({ success: false, message: 'A user with that username already exists.'});
-                else
-                    return res.json(err);
-            }
-
-            res.json({success: true, msg: 'Successfully created new user.'})
-        });
+        db.save(newUser); //no duplicate checking
+        res.json({success: true, msg: 'Successfully created new user.'})
     }
 });
 
 router.post('/signin', function (req, res) {
-    var userNew = new User();
-    userNew.username = req.body.username;
-    userNew.password = req.body.password;
+    let user = db.findOne(req.body.username);
 
-    User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
-        if (err) {
-            res.send(err);
+    if (!user) {
+        res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
+    } else {
+        if (req.body.password === user.password) {
+            let userToken = { id: user.id, username: user.username };
+            let token = jwt.sign(userToken, process.env.SECRET_KEY);
+            res.json ({success: true, token: 'JWT ' + token});
         }
-
-        user.comparePassword(userNew.password, function(isMatch) {
-            if (isMatch) {
-                var userToken = { id: user.id, username: user.username };
-                var token = jwt.sign(userToken, process.env.SECRET_KEY);
-                res.json ({success: true, token: 'JWT ' + token});
-            }
-            else {
-                res.status(401).send({success: false, msg: 'Authentication failed.'});
-            }
-        })
-    })
+        else {
+            res.status(401).send({success: false, msg: 'Authentication failed.'});
+        }
+    }
 });
+
+router.route('/test-collection')
+    .delete(authController.isAuthenticated, function(req, res) {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        let o = getJSONObjectForMovieRequirement(req);
+        res.json(o);
+    }
+    )
+    .put(authJwtController.isAuthenticated, function(req, res) {
+        console.log(req.body);
+        res = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        let o = getJSONObjectForMovieRequirement(req);
+        res.json(o);
+    }
+    );
+
+
+/***********************************************************************************************************************
+ * Movie routing that supports authentication and CRUD operations to a storage interface.
+ * *PUT requires JWT Authentication
+ * *DELETE requires Basic Auth
+ **********************************************************************************************************************/
+router.route('/movies')
+    .post(function(req, res){ // Create
+        console.log("PST| ", req.body);
+        res = res.status(200);
+        let o = getJSONObjectForMovieRequirement(req);
+        // db.saveMovie(movie); | interact with db
+        // check successful db action
+        o.message = "movie saved";
+        res.json(o);
+
+    })
+    .get(function(req, res){ // Retrieve
+        console.log("GET| ", req.body);
+        res = res.status(200)
+        let o = getJSONObjectForMovieRequirement(req);
+        // db.findMovie(movie); | interact with db
+        // check successful db action
+        o.message = "GET movies";
+        res.json(o);
+    })
+    .put(authJwtController.isAuthenticated, function(req, res){ // Update
+        console.log("PUT|", req.body);
+        res = res.status(200);
+        let o = getJSONObjectForMovieRequirement(req);
+        // db.updateMovie(movie); | interact with db
+        // check successful db action
+        o.message = "movie updated";
+        res.json(o);
+    })
+    .delete(authController.isAuthenticated, function(req, res) { // Delete
+        console.log("DEL| ", req.body);
+        res = res.status(200)
+        let o = getJSONObjectForMovieRequirement(req);
+        // db.deleteMovie(movie); | interact with db
+        // check successful db action
+        o.message = "movie deleted";
+        res.json(o);
+    });
+/***********************************************************************************************************************
+ ***********************************************************************************************************************/
+
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
--- test/testcollection.js
+++ test/testcollection.js
@@ -3,7 +3,7 @@ require('dotenv').config({path:envPath});
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
-let User = require('../Users');
+let db = require('../db')();
 chai.should();
 
 chai.use(chaiHttp);
@@ -16,16 +16,12 @@ let login_details = {
 
 describe('Register, Login and Call Test Collection with Basic Auth and JWT Auth', () => {
    beforeEach((done) => { //Before each test initialize the database to empty
-       //db.userList = [];
-
+       db.userList = [];
        done();
     })
 
     after((done) => { //after this test suite empty the database
-        //db.userList = [];
-        User.deleteOne({ name: 'test'}, function(err, user) {
-            if (err) throw err;
-        });
+        db.userList = [];
         done();
     })
 
@@ -36,7 +32,6 @@ describe('Register, Login and Call Test Collection with Basic Auth and JWT Auth'
               .post('/signup')
               .send(login_details)
               .end((err, res) =>{
-                console.log(JSON.stringify(res.body));
                 res.should.have.status(200);
                 res.body.success.should.be.eql(true);
                 //follow-up to get the JWT token
@@ -46,12 +41,34 @@ describe('Register, Login and Call Test Collection with Basic Auth and JWT Auth'
                     .end((err, res) => {
                         res.should.have.status(200);
                         res.body.should.have.property('token');
+
                         let token = res.body.token;
-                        console.log(token);
-                        done();
+                        //console.log('got token ' + token)
+                        //lets call a protected API
+                        chai.request(server)
+                            .put('/testcollection')
+                            .set('Authorization', token)
+                            .send({echo: ''})
+                            .end((err, res) => {
+                                res.should.have.status(200);
+                                res.body.body.should.have.property('echo');
+                                done();
+                            })
                     })
               })
         })
     });
 
+   describe('/testcollection fail auth', () => {
+      it('delete requires basic auth failed login', (done) => {
+          chai.request(server)
+              .delete('/testcollection')
+              .auth('cu_user', 'cu_rulez1')
+              .send({ echo: '' })
+              .end((err, res) => {
+                  res.should.have.status(401);
+                  done();
+              })
+      });
+   });
 });
