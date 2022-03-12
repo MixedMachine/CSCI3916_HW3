58c589d1506bda2835199406857d270eff0a7c87
Michael Marinez <80479622+MixedMachine@users.noreply.github.com>
2022-03-11 19:20:26 -0700

    adding react files

---
+++ web-api-lab/src/reducers/movieReducer.js
@@ -0,0 +1,30 @@
+import constants from '../constants/actionTypes';
+
+let initialState = {
+    movies: [],
+    selectedMovie: null
+}
+
+const movieReducer = (state = initialState, action) => {
+    let updated = Object.assign({}, state);
+
+    switch(action.types) {
+        case constants.FETCH_MOVIES:
+            updated['movies'] = action.movies;
+            updated['selectedMovie'] = action.movies[0];
+            return updated;
+
+        case constants.SET_MOVIE:
+            updated["selectedMovie"] = action.selectedMovie;
+            return updated;
+
+        case constants.FETCH_MOVIE:
+            updated["selectedMovie"] = action.selectedMovie;
+            return updated;
+
+        default:
+            return state;
+    }
+}
+
+export default movieReducer;
