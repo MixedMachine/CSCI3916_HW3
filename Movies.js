0a3bbcd46a36579c1e5b5b83ceec7690d9f9996c
Michael Marinez <80479622+MixedMachine@users.noreply.github.com>
2022-03-13 14:50:20 -0600

    specifying different databases on cluster

--- Movies.js
+++ Movies.js
@@ -4,13 +4,13 @@ let Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
 
 try {
-    mongoose.connect( process.env.DB + process.env.MOVIES_COL, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+    mongoose.connect( `${process.env.DB}${process.env.MOVIES_COL}`, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
         console.log("connected"));
 }catch (error) {
     console.log("could not connect");
 }
 
-//user schema
+// movie schema
 let MovieSchema = new Schema({
     title: { type: String, required: true },
     year: Number,
